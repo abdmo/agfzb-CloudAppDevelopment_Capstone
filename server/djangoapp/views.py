8db205129b758dca693d5bb2bd1995ad750e4f0d
Fattah Zaki <fattahzaki@gmail.com>
2021-09-05 03:48:09 +0000

    Add login and logout functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -27,12 +27,26 @@ def contact(request):
     return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['pswd']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password"
+            return render(request, 'djangoapp:index', context)
+    else:
+        #return render(request, 'djangoapp:index', context)
+        return redirect('djangoapp:index')
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
